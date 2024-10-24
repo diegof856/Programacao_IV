68042311278b12da2499036eba32ae108bf86f42
Diego <diegofelipe1025@gmail.com>
2024-10-24 14:00:15 -0300

    adicionando pasta do projeto

---
+++ Precisao_Tempo/src/main/java/com/programacao_III/Precisao_Tempo/PrecisaoTempoApplication.java
@@ -0,0 +1,13 @@
+package com.programacao_III.Precisao_Tempo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecisaoTempoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrecisaoTempoApplication.class, args);
+	}
+
+}
---
+++ Precisao_Tempo/src/test/java/com/programacao_III/Precisao_Tempo/PrecisaoTempoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programacao_III.Precisao_Tempo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecisaoTempoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
